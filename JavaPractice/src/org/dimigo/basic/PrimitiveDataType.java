28a8f5b674cf61f20249ceac03da703787e3ab27
wp132328 <lime372@gmail.com>
2015-03-17 11:35:30 +0900

    첫번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,27 @@
+package org.dimigo.basic;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		String name = "아이유";
+		boolean sex = true;
+		int age = 23;
+		double height = 161.8;
+		float weight = 44.3f;
+		char BloodType = 'A';
+		
+		System.out.println("이름 : "+name);
+		if (sex) {
+			System.out.println("성별 : 여자");
+		} else {
+			System.out.println("성별 : 남자");
+		}
+		
+		System.out.println("나이 : "+age+" 세");
+		System.out.println("키 : "+height+" cm");
+		System.out.println("몸무게 : "+weight+" kg");
+		System.out.println("혈액형 : "+BloodType+" 형");
+
+	}
+
+}
