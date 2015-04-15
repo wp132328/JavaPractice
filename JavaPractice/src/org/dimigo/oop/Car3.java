85df3909fdf85d3b1a54f4e540ae5a20eb3e2bea
wp132328 <lime372@gmail.com>
2015-04-15 14:49:34 +0900

    일곱 번째 과제 제출

---
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * 	|_ Car
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 14.
+ * </pre>
+ *
+ * @author		: Dummy (WP13 임재홍)
+ * @version		: 1.0
+ */
+public class Car3 {
+
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public Car3() {
+		
+	}
+	
+	public Car3(String company, String model, String color, int maxSpeed, int price) {
+		this.company = company;
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+		this.price = price;
+	}
+	
+	public Car3(String company, String model, String color, int maxSpeed) {
+		this(company, model, color, maxSpeed, 0);
+	}
+	
+	public Car3(String company, String model, String color) {
+		this(company, model, color, 0);
+	}
+	
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String newCompany) {
+		company = newCompany;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int newMaxSpeed) {
+		maxSpeed = newMaxSpeed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int newPrice) {
+		price = newPrice;
+	}
+
+}
