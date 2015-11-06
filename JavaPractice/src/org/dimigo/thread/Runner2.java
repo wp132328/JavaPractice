8674683eacc965a2bd65cda49cb4ecc5f3b6bded
wp132328 <lime372@gmail.com>
2015-11-06 14:31:38 +0900

    실습과제 10

---
+++ JavaPractice/src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * 	|_ Runner
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 6.
+ * </pre>
+ *
+ * @author		: Dummy (WP13 임재홍)
+ * @version		: 1.0
+ */
+public class Runner2 implements Runnable {
+	
+	private String name;
+
+	public Runner2() {
+		
+	}
+	
+	public Runner2(String name) {
+		this.name = name;
+	}
+	
+	public void run() {
+		System.out.println(name + " 출발");
+		for (int i=100; i>0; i-=10) {
+			System.out.println(name + i + " 미터");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(name + " 골인");
+	}
+
+}
